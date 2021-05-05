f7f263cee97eeb7a8bbe8a84816f1bb9bd34834e
AkanWealth <akan.asanga@gmail.com>
2021-05-06 00:40:38 +0100

    working on jwt

--- routes.js
+++ routes.js
@@ -14,7 +14,7 @@ module.exports = (app) => {
     app.get("/is-verify", authorization, isVerify);
     app.post("/signup", validate(schema.signUp), register);
     app.post("/login", validate(schema.login), login);
-    app.get("/dashboard", authorization, dashboard);
+    app.get("/user", authorization, dashboard);
 
     /* Todo section */
     app.get("/todos", todoController.allTodo);
