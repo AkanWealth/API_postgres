b520cec8397374d2d2723adae00e703b4fc7ef78
AkanWealth <akan.asanga@gmail.com>
2021-05-14 00:44:15 +0100

    almost done with contact

---
+++ controllers/contact.js
@@ -0,0 +1,19 @@
+const { pool } = require("../models/db");
+/* Contact section */
+module.exports = {
+    async contact(req, res) {
+        try {
+            let { fullname, email, message } = req.body;
+            const contact = await pool.query(
+                "INSERT INTO contact (fullname,email,message) VALUES ($1, $2, $3) RETURNING *", [fullname, email, message]
+            );
+            res.json({
+                status: "Success",
+                data: contact.rows[0],
+            });
+        } catch (error) {
+            console.log(error.message);
+            res.status(500).send("Server Error");
+        }
+    },
+};
--- routes.js
+++ routes.js
@@ -1,20 +1,16 @@
 const todoController = require("./controllers/todoController");
-const {
-    register,
-    login,
-    isVerify,
-    user,
-} = require("./controllers/usersController");
+const usersController = require("./controllers/usersController");
+const contact = require("./controllers/contact");
 const schema = require("./models/schema");
 const validate = require("./middleware/validation");
 const authorization = require("./middleware/authorization");
 
 module.exports = (app) => {
     /* User section */
-    app.get("/is-verify", authorization, isVerify);
-    app.post("/signup", validate(schema.signUp), register);
-    app.post("/login", validate(schema.login), login);
-    app.get("/user", authorization, user);
+    // app.get("/is-verify", authorization, usersController.isVerify);
+    app.post("/signup", validate(schema.signUp), usersController.register);
+    app.post("/login", validate(schema.login), usersController.login);
+    app.get("/user", authorization, usersController.user);
 
     /* Todo section */
     app.get("/todos", todoController.allTodo);
@@ -22,4 +18,7 @@ module.exports = (app) => {
     app.post("/todos", validate(schema.todos), todoController.insertTodo);
     app.put("/todos/:id", todoController.updateTodo);
     app.delete("/todos/:id", todoController.deleteTodo);
+
+    /* Contact section */
+    app.post("/contactUs", contact.contact);
 };
